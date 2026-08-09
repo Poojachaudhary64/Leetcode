5acaf410fe941e60562ff02df5d9bf6682944183
Poojachaudhary64 <2403051240218@paruluniversity.ac.in>
2026-08-09 23:24:15 +0530

    Time: 0 ms (100%), Space: 48 MB (94.87%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j = s.length -1;
+        while(j>i){
+            swap(s, i, j);
+            i++;
+            j--;
+        }
+    }
+    public void swap(char[] s, int first, int second){
+        char temp = s[first];
+        s[first] = s[second];
+        s[second] = temp;
+    }
+}
